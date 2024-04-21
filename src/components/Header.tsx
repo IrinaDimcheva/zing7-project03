b205ed1073cc34e2f6bf495a9f2eb9224c93971f
Irina Dimcheva <dimchevairina@gmail.com>
2024-04-21 20:32:50 +0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header className="max-w-screen-2xl mx-8 my-4 sm:px-8">
+      <nav className="flex justify-between">
+        <div className="flex gap-6 items-center">
+          <a href="/">
+            <Image src="/bar.svg" alt="menu icon" width={30} height={30} />
+          </a>
+          <a href="/">
+            <Image src="/logo.svg" alt="logo" width={120} height={62} />
+          </a>
+        </div>
+        <ul className="flex justify-between items-center gap-8 font-bold text-[15px] max-lg:hidden">
+          <li>
+            <a href="/">Почивки и екскурзии</a>
+          </li>
+          <li>
+            <a href="/">Хотели</a>
+          </li>
+          <li>
+            <a href="/">Блог</a>
+          </li>
+          <li>
+            <a href="/">За нас</a>
+          </li>
+          <li>
+            <a href="/">Контакти</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
